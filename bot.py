5601fca10491986676e39d7195d8fc02a45632c7
alvincly77 <alvincly77@gmail.com>
2021-10-23 18:28:27 -0700

    update

--- bot.py
+++ bot.py
@@ -9,9 +9,11 @@ env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(,'/slack/events', app)
+slack_event_adapter = SlackEventAdapter(
+  os.environ['SIGNING_SECRET'],'/slack/events', app)
 
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+
 client.chat_postMessage(channel="#test", text="Hello world!")
 
 if __name__ == "__main__":
